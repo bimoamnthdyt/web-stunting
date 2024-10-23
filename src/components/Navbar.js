4c9e3fe68dcefdbde390004f2bcf291ea69d4d45
bimoamnthdyt <bimoamantahidayat01@gmail.com>
2024-10-23 15:32:41 +0700

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
       <h1>Nama Anda</h1>
       <nav>
         <ul>
-          <li><a href="#about">About</a></li>
+          <li><a href="#H">About bintan</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
